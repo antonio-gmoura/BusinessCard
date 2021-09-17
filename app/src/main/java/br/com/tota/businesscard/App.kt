ecc634c06d289d05fc3f5f152a8e2cdf85034d2a
Antonio Moura <antonio.gmoura@gmail.com>
2021-09-17 10:33:41 -0400

    Aula 3 - Configuração do Room Database – Parte 1

--- app/src/main/java/br/com/tota/businesscard/App.kt
+++ app/src/main/java/br/com/tota/businesscard/App.kt
@@ -1,6 +1,10 @@
 package br.com.tota.businesscard
 
 import android.app.Application
+import br.com.tota.businesscard.data.AppDatabase
+import br.com.tota.businesscard.data.BusinessCardRepository
 
 class App : Application() {
+    val database by lazy { AppDatabase.getDatabase(this) }
+    val repository by lazy { BusinessCardRepository(database.businessDao()) }
 }
